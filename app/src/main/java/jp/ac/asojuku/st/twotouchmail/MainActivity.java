d6526f41b0266ea89893a5b4f02be1c69ac946e3
久家政人 <masatokg@masatokg-mba.local>
2016-09-13 14:20:23 +0900

    commit test 20160913

--- app/src/main/java/jp/ac/asojuku/st/twotouchmail/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/st/twotouchmail/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, PickUpActivity.class);
                 startActivity(intent);
+                // version check OK
             }
 
         });
