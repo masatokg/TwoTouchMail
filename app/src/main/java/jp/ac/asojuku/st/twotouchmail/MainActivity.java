bc03c2079ddf2a5679a221211e84f8f2abf865db
久家政人 <masatokg@masatokg-mba.local>
2016-09-13 14:15:01 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/st/twotouchmail/MainActivity.java
@@ -0,0 +1,28 @@
+package jp.ac.asojuku.st.twotouchmail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPickUp = (Button)this.findViewById(R.id.button1);
+        btnPickUp.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, PickUpActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
